5b3d0fd0e6c0b393f09f1ba7bc12d8f6828b2e40
David Russell <git@davidrussell.net>
2022-09-06 17:19:00 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Domain For Sale</title>
+	<meta charset="utf-8" />
+	<meta name="description" content="Domain for sale." />
+	<meta name="HandheldFriendly" content="True" />
+	<meta name="lOptimized" content="width" />
+	<meta http-equiv="cleartype" content="on" />
+	<meta name="viewport" content="width=device-width,minimum-scale=1.0,initial-scale=1,user-scalable=yes">
+	<link rel="stylesheet" href="/styles/screen.css?v=1.0" type="text/css" />
+	<script src="http://www.google.com/jsapi"></script>
+	<script>
+		google.load("jquery", "1.4.2");
+	</script>
+</head>
+<body id="home">
+	<div id="content" class="grid-1">
+		<h1>Domain For Sale</h1>
+		<p>Accepting offers for this domain name. For more information, email the following address. In order to defeat spam bots, this email address will need to be read by a human and typed like a normal email address.</p>
+		<code style="text-align: center; display: block;">
+			&#100;om&#97;i&#110; -a&#116;&#45; &#100;a&#118;i&#100;ru&#115;s&#101;&#108;&#108; &#45;&#100;o&#116;- &#111;r&#103;
+		</code>
+	</div>
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-89672-1']);
+		_gaq.push(['_trackPageview']);
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+</body>
+</html>
